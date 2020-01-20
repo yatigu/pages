d98886488d2e211531642cdce1086bfe816e407f
kwtjdtn <kwtjdtn1@naver.com>
2020-01-20 15:30:15 +0900

    add new page

--- src/component/Reservation.js
+++ src/component/Reservation.js
@@ -4,7 +4,7 @@ import  './Reservation.css';
 const Reservation = () => {
     return (
         <div className = "but-wrapper">
-            <input type = 'button' className = "res-but" value = "예약하기"/>
+            <input type = 'button' className = "res-but" value = "기차 조회하기"/>
         </div>
     )
 }
